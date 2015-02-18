4d7f82d29020ea6655d21a1817e3bbd758cff988
hhklmc <harunkilimci@outlook.com>
2015-02-18 17:41:27 +0000

    Update Line.swift

--- TheDraw/Line.swift
+++ TheDraw/Line.swift
@@ -12,11 +12,13 @@ class Line {
     var start: CGPoint
     var end: CGPoint
     var color: UIColor
+    var lineWidth: CGFloat //adding spesific line width for each line
     
-    init(start _start: CGPoint, end _end: CGPoint, color _color: UIColor!) {
+    init(start _start: CGPoint, end _end: CGPoint, color _color: UIColor!, lineWidth _lineWidth: CGFloat) {
     //init(start _start: CGPoint, end _end: CGPoint) {
         start = _start
         end = _end
         color = _color
+        lineWidth = _lineWidth
     }
 }
